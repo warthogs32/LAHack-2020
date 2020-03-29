6f2dce1cd37a4bf7a411fa6e5af04c85d4817ddc
jason <jj@jj-jj.org>
2020-03-28 20:24:54 -0700

    patient record controller

--- Challenge 1 - Patient Match/PatientMatching/PatientMatching/Controllers/PatientController.cs
+++ Challenge 1 - Patient Match/PatientMatching/PatientMatching/Controllers/PatientController.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Http.Description;
+using PatientMatching.DTOs;
+using System.Web.Cors;
 using System.Web.Http;
+using PatientMatching.Repositories;
+using PatientMatching.Models;
 
 namespace PatientMatching.Controllers
 {
+    [RoutePrefix("api/patients")]
     public class PatientController : ApiController
     {
+        private PatientRepository _patientRepo = new PatientRepository();
+
+        [Route("getAllRecords/")]
+        [ResponseType(typeof(GetAllPatientRecordsResponseDTO))]
+        [HttpGet]
+        public GetAllPatientRecordsResponseDTO GetAllEvents()
+        {
+            List<PatientRecord> allPatients = _patientRepo.GetAllPatientRecords();
+            return new GetAllPatientRecordsResponseDTO()
+            {
+                Patients = allPatients
+            };
+
+        }
     }
 }
---
+++ Challenge 1 - Patient Match/PatientMatching/PatientMatching/DTOs/GetAllPatientRecordsResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PatientMatching.Models;
+
+namespace PatientMatching.DTOs
+{
+    public class GetAllPatientRecordsResponseDTO
+    {
+        public List<PatientRecord> Patients { get; set; }
+    }
+}
